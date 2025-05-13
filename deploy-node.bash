d0a1ec411de2f1dc01fd8f7585693776175db0b9
MiningCryptoLive <celldr69@gmail.com>
2025-05-13 10:37:43 -0400

    Create deploy-node.bash

---
+++ deploy-node.bash
@@ -0,0 +1,9 @@
+DEBIAN_FRONTEND=noninteractive apt-get install -y git make g++ cmake libssl-dev libunbound-dev libboost-dev libboost-system-dev libboost-date-time-dev libboost-dev libboost-system-dev libboost-date-time-dev libboost-filesystem-dev libboost-thread-dev libboost-chrono-dev libboost-locale-dev libboost-regex-dev libboost-regex-dev libboost-program-options-dev libzmq3-dev
+cd /usr/local/src
+git clone https://github.com/monero-project/monero.git
+cd monero
+git checkout v0.18.3.4
+git submodule update --init
+USE_SINGLE_BUILDDIR=1 make -j$(nproc) release || USE_SINGLE_BUILDDIR=1 make -j1 release
+
+(cat <<EOF
